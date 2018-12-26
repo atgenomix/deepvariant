32edb5e9193d01cb05332bd0e09c38f521905978
A-Tsai <chungtsai@gmail.com>
2018-12-26 12:11:16 +0800

    housekeeping for tarball deletion.

--- run-prereq.sh
+++ run-prereq.sh
@@ -158,12 +158,14 @@ if [[ "${DV_GPU_BUILD}" = "1" ]]; then
         sudo -H dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
         sudo -H apt-get update
         sudo -H apt-get -y install cuda-9-0
+        sudo -H rm -rf ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
       else
         wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
         sudo -H dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
         sudo -H apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
         sudo -H apt-get update
         sudo -H apt-get install -y cuda-libraries-9-0
+        sudo -H rm -rf cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
       fi
     fi
 
@@ -177,6 +179,7 @@ if [[ "${DV_GPU_BUILD}" = "1" ]]; then
       sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-9.0/lib64/
       sudo chmod a+r /usr/local/cuda-9.0/lib64/libcudnn*
       sudo ldconfig
+      sudo rm -rf ${CUDNN_TAR_FILE}
     fi
 
     # Tensorflow says to do this.
